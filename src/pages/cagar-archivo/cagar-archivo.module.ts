d64e847c3841b2e8154c84865379c2b2183552cc
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2017-11-16 14:28:05 -0300

    se agrega pace carga-archivo

---
+++ src/pages/cagar-archivo/cagar-archivo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CagarArchivoPage } from './cagar-archivo';
+
+@NgModule({
+  declarations: [
+    CagarArchivoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CagarArchivoPage),
+  ],
+})
+export class CagarArchivoPageModule {}
