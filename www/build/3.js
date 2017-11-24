594c90d931a378d4cd03a931fa9b8a62dac6109a
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2017-11-24 20:42:36 -0300

    mod

--- www/build/3.js
+++ www/build/3.js
@@ -1,14 +1,14 @@
 webpackJsonp([3],{
 
-/***/ 576:
+/***/ 579:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MateriasPageModule", function() { return MateriasPageModule; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NotificacionPageModule", function() { return NotificacionPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__materias__ = __webpack_require__(108);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__notificacion__ = __webpack_require__(190);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,23 +18,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var MateriasPageModule = (function () {
-    function MateriasPageModule() {
+var NotificacionPageModule = (function () {
+    function NotificacionPageModule() {
     }
-    return MateriasPageModule;
+    return NotificacionPageModule;
 }());
-MateriasPageModule = __decorate([
+NotificacionPageModule = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["L" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__materias__["a" /* MateriasPage */],
+            __WEBPACK_IMPORTED_MODULE_2__notificacion__["a" /* NotificacionPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__materias__["a" /* MateriasPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__notificacion__["a" /* NotificacionPage */]),
         ],
     })
-], MateriasPageModule);
+], NotificacionPageModule);
 
-//# sourceMappingURL=materias.module.js.map
+//# sourceMappingURL=notificacion.module.js.map
 
 /***/ })
 
